afdd8a7d608d587437cb25feb820b7c052c92a8b
Laura Tozzo <109946485+LaTozzo@users.noreply.github.com>
2022-09-22 09:47:36 +0200

    Update algorithms.py

--- subengines/demandForecasting/algorithms.py
+++ subengines/demandForecasting/algorithms.py
@@ -121,3 +121,6 @@ def prophet_forecast(df_train, df_expost, df_pred, algorithm_params, historical_
         result["err_message"] = f"Algorithm {algorithm_name} failed to calculate forecast. Error: {e}"
 
     return result
+
+#SPDX-FileCopyrightText: 2022 SAP SE
+#SPDX-License-Identifier: Apache-2.0
